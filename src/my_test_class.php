43412472c345c020f26ed5b751fc2382440d4150
pedde07 <senf_glas@hotmail.com>
2018-12-31 16:59:27 +0100

    Create my_test_class.php

---
+++ src/my_test_class.php
@@ -0,0 +1,9 @@
+namespace MyTestNameSpace;
+
+class my_test_class {
+
+  public static function MyTestFunction() {
+    echo '### This is function '.__FUNCTION__.' of class '.__CLASS__.' ###'.PHP_EOL;
+  }
+
+}
